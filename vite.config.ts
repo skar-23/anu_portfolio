f3e6adbabfc5fbcc9d16e90971ca74f339d237ae
sai Kiran <saikiran@192.168.1.3>
2026-03-26 21:05:55 +0530

    set base path to / for vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/anu_portfolio/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
